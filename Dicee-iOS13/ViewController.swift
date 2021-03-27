63c23a4ca99d9f366941e28b7a2b5af87f07dfc6
Blake Sowers <blake@BSDevelopment.dev>
2021-03-26 19:25:02 -0600

    test push

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DiceImageView1: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
